a30d031b821040a01165d57fb0ade5e6cf367671
Starr Horne <starr@chromahq.com>
2010-05-30 11:17:27 -0500

    updated sample adapter to use data accessor

--- config/settings/adapters/sample_adapter.rb
+++ config/settings/adapters/sample_adapter.rb
@@ -1,9 +1,13 @@
 class SampleAdapter < Konfig::Adapter
 
+  # The adapter parent class provides an accessor 'data' which contains
+  # the raw settings data.
+  #
   # Do whatever you want with the data here. You could use it to set up
-  # a 3rd party plugin, or to modify the config data.
-  # @param [Hash] data The raw settings data
-  def adapt(data)
+  # a 3rd party plugin. You an also modify the data itself. Changes to 
+  # data will be available to the rest of the application.
+  #
+  def adapt
     data[:sample_adapter] = :sample_adapter
   end
 
